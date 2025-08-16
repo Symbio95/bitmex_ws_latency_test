2c1c6a33485949521039225f9350a1b1f149668e
Symbio95 <delibasic.x@gmail.com>
2025-08-16 18:30:17 +0200

    w

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,7 @@ async fn main() {
     let mut start = Utc::now();
     while let Some(_) = reader.next().await {
             let latency = Utc::now().signed_duration_since(start).num_microseconds().unwrap() as f64 / 1000.0;
-            
+            println!("latency: {:.3} ms", latency);
             if latency > 10.0 {
                 continue;
             }
